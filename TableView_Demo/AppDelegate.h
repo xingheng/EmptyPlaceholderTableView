8cc9c221b8756c0a0a114dac8098255ba92a0c11
Han Wei <hanwei@huantengsmart.com>
2014-09-12 15:09:01 +0800

    Initial Commit

---
+++ TableView_Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableView_Demo
+//
+//  Created by WeiHan on 9/12/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableView_Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableView_Demo
+//
+//  Created by WeiHan on 9/12/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
